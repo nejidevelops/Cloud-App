bceac26215faad9b511ea8f0798a0816307d8465
Newton Ombese <newtonombese1@gmail.com>
2022-11-25 09:01:32 +0300

    adding my needed images

---
+++ src/components/Hero.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {CloudUploadIcon, DatabaseIcon, PaperAirplaneIcon, ServerIcon} from '@heroicons/react/solid'
+
+function Hero() {
+  return (
+    <div>Hero</div>
+  )
+}
+
+export default Hero
