6baea6a0dfaa55be0e54cdb18718cf2c6f908c1c
Newton Ombese <newtonombese1@gmail.com>
2022-11-25 08:37:44 +0300

    working on the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,15 @@ import {MenuIcon, XIcon} from '@heroicons/react/outline';
 
 function Navbar() {
   return (
-    <div>
-        <MenuIcon />
+    <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
+        <div className='px-2 flex justify-between items-center w-full h-full'>
+            <div className='flex items-center'>
+                <h1 className='text-3xl font-bold mr-4'>BRAND.</h1>
+            </div>
+        </div>
+        
+        
+        <MenuIcon className='w-5' />
     </div>
   )
 }
