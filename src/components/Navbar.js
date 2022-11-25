7c697370eff87ddfaed1e896eb1212abd1c24a25
Newton Ombese <newtonombese1@gmail.com>
2022-11-25 08:33:59 +0300

    resolved the hero icon version issue

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import {MenuIcon, XIcon} from '@heroicons/react/outline';
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+        <MenuIcon />
+    </div>
   )
 }
 
