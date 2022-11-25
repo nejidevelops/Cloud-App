d9a1c02aab31c58f968f0b454994d692dfc613cc
Newton Ombese <newtonombese1@gmail.com>
2022-11-25 09:37:35 +0300

    finishing on the data services

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,7 +13,16 @@ function Hero() {
                 <button className='py-3 px-6 sm:w-[60%] my-4'>Get Started</button>
             </div>
             <div>
-                
+                <img className='w-full' src={backgroundImg} alt="/" />
+            </div>
+            <div className='absolute flex flex-col py-7 md:min-w-[760px] bottom-[5%] mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-zinc-200 border border-slate-300 rounded-xl text-center shadow-xl'>
+                <p>Data Services</p>
+                <div className='flex justify-between flex-wrap px-4'>
+                    <p className='flex px-4 py-2 text-slate-500'><CloudUploadIcon  className='h-6 text-indigo-500' /> App Security</p>
+                    <p className='flex px-4 py-2 text-slate-500'><DatabaseIcon  className='h-6 text-indigo-500' /> Dashboard Design</p>
+                    <p className='flex px-4 py-2 text-slate-500'><ServerIcon className='h-6 text-indigo-500' /> Cloud Data</p>
+                    <p className='flex px-4 py-2 text-slate-500'><PaperAirplaneIcon  className='h-6 text-indigo-500' /> API</p>
+                </div>
             </div>
         </div>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
     <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
         <div className='px-2 flex justify-between items-center w-full h-full'>
             <div className='flex items-center'>
-                <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>BRAND.</h1>
+                <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>BOFULO.</h1>
                 <ul className='hidden md:flex'>
                     <li>Home</li>
                     <li>About</li>
